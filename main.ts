29738e0c4bd433bda77be0f144e35abb0e2fc131
GÜNAY GÜZEL <102692490+guguz58@users.noreply.github.com>
2025-03-27 14:30:58 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,42 @@
+input.onButtonPressed(Button.A, function () {
+    strip.showRainbow(1, 360)
+    strip.show()
+})
+input.onButtonPressed(Button.B, function () {
+    strip.clear()
+    neopixel.create(DigitalPin.P12, strip, NeoPixelMode.RGB).clear()
+})
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P12, 60, NeoPixelMode.RGB)
+basic.showLeds(`
+    . . . . .
+    # . . . .
+    # . . . .
+    . # . . .
+    . . # # .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . #
+    . . . . #
+    . . . # .
+    . # # . .
+    `)
+basic.showIcon(IconNames.Heart)
+huskylens.initI2c()
+huskylens.initMode(protocolAlgorithm.OBJECTCLASSIFICATION)
+basic.forever(function () {
+    huskylens.request()
+    if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        huskylens.writeName(1, "cam")
+        huskylens.writeOSD("cam", 150, 30)
+    }
+    if (huskylens.isAppear(2, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        huskylens.writeName(2, "kagit")
+        huskylens.writeOSD("kagit", 150, 30)
+    }
+    if (huskylens.isAppear(3, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        huskylens.writeName(3, "pet")
+        huskylens.writeOSD("atık yok", 150, 30)
+    }
+})
